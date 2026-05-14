d538ab71a59c7e37422a29714e4dd7e4f90b9f65
mrk luffy <helpself87@gmail.com>
2026-05-14 10:59:11 +0200

    Grade R

--- resources/views/school-admin-dashboard/index.blade.php
+++ resources/views/school-admin-dashboard/index.blade.php
@@ -759,14 +759,15 @@ canvas {
     <h2>Filters</h2>
     <hr class="filter-separator"/>
     <form method="GET" action="{{ url()->current() }}" class="filters" id="filtersForm">
-        <select name="abuse_type" onchange="this.form.submit()">
+       <select name="abuse_type" onchange="this.form.submit()">
             <option value="">Any Reports Type</option>
-            @foreach ($abuseTypes as $type)
+            @foreach ($abuseTypes->sortBy('type_name') as $type)
                 <option value="{{ $type->id }}" {{ $abuseTypeFilter == $type->id ? 'selected' : '' }}>
                     {{ $type->type_name }}
                 </option>
             @endforeach
         </select>
+
         <select name="age_range" onchange="this.form.submit()">
             <option value="">Any Age</option>
             @foreach (['0-10','11-15','16-20','21-22'] as $range)
